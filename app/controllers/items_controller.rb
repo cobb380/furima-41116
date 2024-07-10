94fb752671be3c0c84f3378fe421718260a0f3b4
cobb380 <cobbsalad380@gmail.com>
2024-07-10 17:39:52 +0900

    商品編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :redirect_if_not_owner, only: [:new, :create, :edit, :update]
 
   def new
     @item = Item.new
@@ -23,10 +23,29 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :postage_id, :prefecture_id, :until_shipping_id, :price, :image).merge(user_id: current_user.id)
   end
 
+  def redirect_if_not_owner
+    @item = Item.find(params[:id])
+    unless @item.user_id == current_user.id
+      redirect_to root_path
+    end
+  end
 end
